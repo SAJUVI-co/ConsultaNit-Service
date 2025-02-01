68b28ad3cb55c2fb9a8b9639d91b7999d83da94b
CODE-G-ROOT <juanavila8856@gmail.com>
2025-01-31 21:49:27 -0500

    fix: :bug: update message pattern command to use CONSULTANIT_CMD in ConsultaController

--- src/consulta/consulta.controller.ts
+++ src/consulta/consulta.controller.ts
@@ -1,12 +1,12 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { ConsultaService } from './consulta.service';
-import { CONSULTANIT_HOST } from 'src/config/env.config';
+import { CONSULTANIT_CMD } from 'src/config/env.config';
 
 @Controller()
 export class ConsultaController {
   constructor(private readonly consultaService: ConsultaService) {}
-  @MessagePattern({ cmd: CONSULTANIT_HOST })
+  @MessagePattern({ cmd: CONSULTANIT_CMD })
   findOne(@Payload('cc') cc: string) {
     const data = this.consultaService.findOne(cc);
     return data;
