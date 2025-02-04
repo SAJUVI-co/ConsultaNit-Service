a747103a11f5c09991cb787afdb655fee38bb6b7
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-03 23:36:59 -0500

    feat: :sparkles: update environment variables

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -3,7 +3,8 @@ import * as joi from 'joi';
 
 interface Envs {
   CONSULTANIT_HOST: string;
-  CONSULTANIT_CMD: string;
+  CONSULTANIT_CMD_NIT: string;
+  CONSULTANIT_CMD_NITS: string;
   CONSULTANIT_PORT: number;
 }
 
@@ -11,7 +12,8 @@ const schema = joi
   .object({
     CONSULTANIT_HOST: joi.string().required(),
     CONSULTANIT_PORT: joi.number().required(),
-    CONSULTANIT_CMD: joi.string().required(),
+    CONSULTANIT_CMD_NIT: joi.string().required(),
+    CONSULTANIT_CMD_NITS: joi.string().required(),
   })
   .unknown(true);
 
@@ -21,5 +23,9 @@ if (data.error) {
   throw new Error(`Config validation error: ${data.error.message}`);
 }
 
-export const { CONSULTANIT_HOST, CONSULTANIT_PORT, CONSULTANIT_CMD } =
-  data.value as Envs;
+export const {
+  CONSULTANIT_HOST,
+  CONSULTANIT_PORT,
+  CONSULTANIT_CMD_NIT,
+  CONSULTANIT_CMD_NITS,
+} = data.value as Envs;
