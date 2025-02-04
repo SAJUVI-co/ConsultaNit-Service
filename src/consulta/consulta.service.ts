0dae1b425b542a230a1d3e5dbd9a8ec3b40888fa
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-04 01:09:39 -0500

    feat: :sparkles: update command to use CONSULTANIT_CMD_NIT and replace query.vbs with consulta_nit.vbs in ConsultaService

--- src/consulta/consulta.service.ts
+++ src/consulta/consulta.service.ts
@@ -4,16 +4,20 @@ import { exec } from 'child_process';
 @Injectable()
 export class ConsultaService {
   private search_nit(cc: string): Promise<string> {
+    console.log(cc);
     return new Promise((resolve, reject) => {
-      exec(`cscript ../query.vbs ${cc}`, (error, stdout, stderr) => {
-        if (error) {
-          reject(new Error(`Error ejecutando el script: ${error.message}`));
-        } else if (stderr) {
-          reject(new Error(`Error del script: ${stderr}`));
-        } else {
-          resolve(stdout.trim());
-        }
-      });
+      exec(
+        `cscript ./src/consulta/consulta_nit.vbs ${cc}`,
+        (error, stdout, stderr) => {
+          if (error) {
+            reject(new Error(`Error ejecutando el script: ${error.message}`));
+          } else if (stderr) {
+            reject(new Error(`Error del script: ${stderr}`));
+          } else {
+            resolve(stdout.trim());
+          }
+        },
+      );
     });
   }
 
